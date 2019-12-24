c9d16cf754481c64d41dd66c018d423e48ddb39b
fonta90 <andrea.fontana90@gmail.com>
2019-12-24 17:51:34 +0100

    adding notebook for the Capstone Project

---
+++ Notebook-for-Capstone-Project.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone project for IBM Data Science Professional Certificate
+
+# *This Notebook will be mainly used for the Capstone Project of IBM Data Science Professional Certificate offered through Coursera.*
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
